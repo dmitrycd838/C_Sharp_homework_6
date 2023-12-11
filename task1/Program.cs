8c20b5838f69994dfb75e47ecf32f3f3b77a70c5
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-12-11 11:20:04 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+
+char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+
+string result = CreateStringFrom2DArray(charArray);
+
+System.Console.WriteLine(result);
+
+
+string CreateStringFrom2DArray(char[,] array)
+{
+    string result = "";
+    for ( int i = 0; i < array.GetLength(0); i++)
+    {
+        for ( int j = 0; j < array.GetLength(1); j++)
+        {
+            result += array[i, j];
+        }
+    }
+    return result;
+}
+
