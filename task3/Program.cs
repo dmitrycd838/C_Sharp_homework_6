3d234b3eee4b66af92646b4b4231dd6eb324578d
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-12-11 11:37:09 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string input = "шалаш";
+
+// Вызов метода для проверки, является ли строка палиндромом 
+bool isPalindrome = IsPalindrome(input);
+
+System.Console.WriteLine(isPalindrome ? "Да" : "Нет");
+
+// Метод для проверки, является ли строка палиндромомpu
+bool IsPalindrome(string str)
+{
+    // Нормализация строки путем удаления не буквенно-цифровых символов и приведения к нижнему регистру
+    string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    // Сравнение строки с ее перевернутым вариантом
+    return normalized.SequenceEqual(normalized.Reverse());
+}
